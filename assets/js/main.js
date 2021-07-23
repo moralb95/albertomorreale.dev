a7d13a18b68ae4fe46e9ff773f22e813708b6e47
Alberto Morreale <albertomorreale95@gmail.com>
2021-07-23 15:54:49 +0200

    navbar and main-section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,34 @@
 $(window).scroll(function () {
     $('#main-nav').toggleClass('navbar-scrolled', window.scrollY !== 0);
 });
+
+function scrollSmoothTo(elementId) {
+    var element = document.getElementById(elementId);
+    element.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth'
+    });
+
+    return false;
+}
+
+const phrases = [
+    'I\'m Alberto Morreale',
+    'I come from Sicily',
+    'I\'m a Web Developer',
+]
+
+$(document).ready(function () {
+    const el = document.getElementById("messenger");
+    const fx = new TextScramble(el);
+
+    let counter = 0
+    const next = () => {
+        fx.setText(phrases[counter]).then(() => {
+            setTimeout(next, 2200)
+        })
+        counter = (counter + 1) % phrases.length
+    }
+
+    next();
+});
